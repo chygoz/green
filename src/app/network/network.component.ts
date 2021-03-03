ecb2112bdcdb9ff0b3ba250a8b08b9cba9103101
sainik18 <sainik18@gmail.com>
2021-03-04 00:52:14 +0530

    network page

--- src/app/network/network.component.ts
+++ src/app/network/network.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { apiService } from '../api.service';
+import { CookieService } from '../services/cookie.service';
 
 @Component({
   selector: 'app-network',
@@ -7,12 +8,23 @@ import { apiService } from '../api.service';
   styleUrls: ['./network.component.scss']
 })
 export class NetworkComponent implements OnInit {
+  connections: Number = 0;
+  userData = '';
 
-  constructor(private service: apiService) { }
+  constructor(private service: apiService, private cookieService: CookieService) { 
+    let ud = this.cookieService.getCookie('currentUser');
+    this.userData = JSON.parse(ud);
+    console.log(ud);
+  }
 
   ngOnInit(): void {
-    this.service.getUserNetwork({}).subscribe((data) => {
-      console.log(data);
+    this.service.getUserNetwork({}).subscribe((resp) => {
+      console.log(resp);
+      if(resp.status && resp.data.length > 0){
+        this.connections = resp.data.length
+      }else {
+
+      }
     })
   }
 
