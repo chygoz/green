289e30e558c031fc54398be2d69eaf4a9a4c708a
varavk <varavk@gmail.com>
2021-03-24 19:49:04 +0530

    bug fixing

--- src/app/landing-header/landing-header.component.ts
+++ src/app/landing-header/landing-header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from '../services/cookie.service';
+import { apiService } from '../api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-header',
@@ -6,15 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-header.component.scss']
 })
 export class LandingHeaderComponent implements OnInit {
+  userData;
+  loginStatus = false;
+  constructor(private router: Router,
+    private cookieService: CookieService, private service: apiService) {
+    this.userData = this.cookieService.getCookie('currentUser');
+    this.userData = JSON.parse(this.userData);
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
+    if (this.userData) {
+      this.loginStatus = true;
+    }
+
   }
 
   navigateToSection(section: string) {
     window.location.hash = '';
     window.location.hash = section;
+  }
+
+  logout() {
+    this.cookieService.deleteCookie('currentUser');
+    this.cookieService.deleteCookie('token');
+    this.router.navigate(['/login']);
   }
 
 }
