88b1cd579c16c7ffd91f9adbe9dc3f0a7c096d8e
sainik18 <sainik18@gmail.com>
2021-04-06 00:16:38 +0530

    Loaded added

--- src/app/loader/loader.component.ts
+++ src/app/loader/loader.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.scss']
 })
-export class LoaderComponent implements OnInit {
-
+export class LoaderComponent implements OnInit, AfterViewInit {
+  intervalId = 0;
+  seconds = 30;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  ngAfterViewInit() {
+    console.log("called")
+    this.countDown();
+  }
+  
+
+  private countDown() {
+    this.clearTimer();
+    this.intervalId = window.setInterval(() => {
+      this.seconds -= 1;
+      if (this.seconds === 0) {
+        //this.message = 'Blast off!';
+        this.clearTimer();
+      } else {
+        //if (this.seconds < 0) { this.seconds = 10; } // reset
+        //this.message = `T-${this.seconds} seconds and counting`;
+      }
+    }, 1000);
+  }
+
+  clearTimer() { clearInterval(this.intervalId); }
 
 }
